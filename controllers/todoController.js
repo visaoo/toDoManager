47d6c2249a26c95e501170f60de374186b38f508
--global <--global>
2025-08-16 00:14:10 -0300

    fix: parametros do todoController e routas

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,86 +1,129 @@
+const database = require('../models/connect'),
+	{ request, response } = require('express')
+
 /**
  * Cria uma nova tarefa.
- * @param {{ title: string, description: string, userId: string }} params 
- * @returns retorna o ID da nova tarefa
+ * @param {request} req
+ * @param {response} res
  */
-async function createTask(params) {
-  const { title, description, userId } = params;
+async function createTask(req, res) {
+	const { title, description, userId } = req.body
 
-  if (!title || !description || !userId) {
-    throw new Error('Todos os campos são obrigatórios');
-  }
+	if (!title || !description || !userId) {
+		return res.status(400).json({ error: 'Todos os campos são obrigatórios' })
+	}
 
-  const result = await database.query('INSERT INTO tarefas (title, description, user_id) VALUES (?, ?, ?)', [title, description, userId]);
-  return result[0].insertId;
+	const result = await database.query(
+		'INSERT INTO tarefas (titulo, descricao, usuario_id) VALUES (?, ?, ?)',
+		[title, description, userId]
+	)
+	return res.status(201).json({ id: result[0].insertId, message: 'Tarefa criada com sucesso' })
 }
 
 /**
  * Deleta uma tarefa existente.
- * @param {string} id 
- * @returns retorna um booleano indicando se a exclusão foi bem-sucedida
+ * @param {request} req
+ * @param {response} res
  */
-async function deleteTask(id) {
+async function deleteTask(req, res) {
+	const { id } = req.body;
+  const userId = req.user;
 
   if (!id) {
-    throw new Error('O ID do todo é obrigatório');
+		return res.status(400).json({ error: 'O ID da tarefa é obrigatório' })
+	}
+  if (!userId) {
+    return res.status(401).json({ error: 'Usuário não autenticado' });
   }
 
-  const result = await database.query('DELETE FROM tarefas WHERE id = ?', [id]);
-  return result.affectedRows > 0;
+  const [taskOwner] = await database.query('SELECT usuario_id FROM tarefas WHERE id = ?', [id]);
+
+  if (!taskOwner || taskOwner[0].usuario_id != userId) {
+    return res.status(403).json({ error: 'Acesso negado' });
+  }
+
+	// const result = await database.query('DELETE FROM tarefas WHERE id = ?', [id])
+	return res.status(200).json({ message: 'Tarefa deletada com sucesso' })
 }
+
 /**
  * Obtém todas as tarefas de um usuário específico.
- * @param {string} userId 
- * @returns retorna uma lista de tarefas
+ * @param {request} req
+ * @param {response} res
  */
-async function getTasksByUserId(userId) {
-  if (!userId) {
-    throw new Error('O ID do usuário é obrigatório');
-  }
-
-  const result = await database.query('SELECT * FROM tarefas WHERE user_id = ?', [userId]);
-  return result;
+async function getTasksByUserId(req, res) {
+	const userId = req.user
+	const [result] = await database.query('SELECT * FROM tarefas WHERE usuario_id = ?', [userId])
+  
+	return res.status(200).json(result)
 }
 
 /**
  * Atualiza uma tarefa existente.
- * @param {{ title: string, description: string, status: string }} params 
- * @returns retorna um booleano indicando se a atualização foi bem-sucedida
+ * @param {request} req
+ * @param {response} res
+ * @returns 
  */
 
-async function updateTask(params) {
-  const { title, description, status } = params;
+async function updateTask(req, res) {
+	const { id, title, description, concluido } = req.body
+  const user = req.user
 
-  const fields = [];
-  const values = [];
-
-  if (title) {
-    fields.push('title = ?');
-    values.push(title);
+  if (!user) { // teoricamente, isso nunca deve acontecer
+    return res.status(401).json({ error: 'Usuário não autenticado' })
   }
 
-  if (description) {
-    fields.push('description = ?');
-    values.push(description);
-  }
+  const [taskOwner] = await database.query('SELECT usuario_id FROM tarefas WHERE id = ?', [id])
 
-  if (status) {
-    fields.push('status = ?');
-    values.push(status);
-  }
+	if (!taskOwner || taskOwner[0].usuario_id != user) {
+		return res.status(403).json({ error: 'Acesso negado' })
+	}
 
-  if (fields.length === 0) {
-    throw new Error('Nenhum campo para atualizar');
-  }
+	const fields = []
+	const values = []
 
-  const result = await database.query(`UPDATE tarefas SET ${fields.join(', ')} WHERE id = ?`, [...values, id]);
-  return result.affectedRows > 0;
+	if (title) {
+		fields.push('titulo = ?')
+		values.push(title)
+	}
+
+	if (description) {
+		fields.push('descricao = ?')
+		values.push(description)
+	}
+
+	if (concluido) {
+		fields.push('concluido = ?')
+		values.push(concluido)
+	}
+
+	if (fields.length === 0) {
+		return res.status(400).json({ message: 'Nenhum campo para atualizar' })
+	}
+
+	const [result] = await database.query(`UPDATE tarefas SET ${fields.join(', ')} WHERE id = ?`, [
+		...values,
+		id,
+	])
+	return res.status(200).json({ message: 'Tarefa atualizada com sucesso' })
 }
 
+async function getTaskOwner(id, userId) {
+  if (!id) {
+    throw new Error('O ID da tarefa é obrigatório');
+  }
+  if (!userId) {
+    throw new Error('O ID do usuário é obrigatório');
+  }
+  
+  const taskOwner = await database.query('SELECT usuario_id FROM tarefas WHERE id = ?', [id])
+
+  return taskOwner
+}
 
 module.exports = {
-  createTask,
-  deleteTask,
-  getTasksByUserId,
-  updateTask
-};
+	createTask,
+	deleteTask,
+	getTasksByUserId,
+	updateTask,
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,20 @@
 const { Router } = require("express"),
   router = Router(),
   userController = require("../controllers/userController"),
-  authController = require('../controllers/authController');
+  authController = require('../controllers/authController'),
+  todoController = require('../controllers/todoController'),
+  authMiddleware = require('../utils/AuthMiddleware');
 
 router.get("/usuarios", userController.listUsers);
 router.post("/usuarios", userController.createUser);
 router.post("/login", authController.authenticateUser);
+
+// Rotas das tarefas
+router.post("/tarefas", authMiddleware, todoController.createTask);
+router.get("/tarefas", authMiddleware, todoController.getTasksByUserId);
+
+
+router.put("/tarefas", authMiddleware, todoController.updateTask);
+router.delete("/tarefas", authMiddleware, todoController.deleteTask);
 
 module.exports = router;
